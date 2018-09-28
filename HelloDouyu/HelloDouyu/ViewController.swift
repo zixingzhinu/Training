27d881d7627fd48d400609c5f7f068975e3eab1c
James <liujian01@finupgroup.com>
2018-09-28 14:35:08 +0800

    init

---
+++ HelloDouyu/HelloDouyu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloDouyu
+//
+//  Created by James on 2018/9/27.
+//  Copyright © 2018 FinupCredit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
---
+++ HelloDouyu/HelloDouyuTests/HelloDouyuTests.swift
@@ -0,0 +1,34 @@
+//
+//  HelloDouyuTests.swift
+//  HelloDouyuTests
+//
+//  Created by James on 2018/9/27.
+//  Copyright © 2018 FinupCredit. All rights reserved.
+//
+
+import XCTest
+@testable import HelloDouyu
+
+class HelloDouyuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
