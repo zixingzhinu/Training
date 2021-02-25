da4b36adcbca633d64c49be996beaa623cc0f2d2
James <liujian01@renmaitech.com>
2021-02-25 17:34:24 +0800

    wanandroid新项目

---
+++ WanAndroidSwift/WanAndroidSwift/Main/WANavigationController.swift
@@ -0,0 +1,12 @@
+//
+//  WANavigationController.swift
+//  WanAndroidSwift
+//
+//  Created by James on 2021/2/23.
+//
+
+import UIKit
+
+class WANavigationController: UINavigationController {
+    
+}
---
+++ WanAndroidSwift/WanAndroidSwift/Main/WATabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  WATabBarController.swift
+//  WanAndroidSwift
+//
+//  Created by James on 2021/2/25.
+//
+
+import UIKit
+
+class WATabBarController: UITabBarController {
+    
+    func getChildViewController() -> ViewController {
+        <#function body#>
+    }
+}
